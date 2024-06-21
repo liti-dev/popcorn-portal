bef8b015531f7815805cb8e3ed18592d61030d21
Tia Nguyen <tia.nkm03@gmail.com>
2024-06-21 18:47:04 +0100

    tidy up

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-import React, { useState } from "react"
+import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import StarRatings from "./components/StarRatings"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRatings maxRating={10} color="#bed558" />
-    <Test />
+    <App />
   </React.StrictMode>
 )
-function Test() {
-  const [rating, setMovieRating] = useState(0)
-  return (
-    <>
-      <StarRatings setMovieRating={setMovieRating} maxRating={5} />
-      <p>It was rated {rating} stars</p>
-    </>
-  )
-}
