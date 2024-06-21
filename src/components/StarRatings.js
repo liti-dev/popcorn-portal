8100257f95773fe44cd759c582df90c311b32b20
Tia Nguyen <tia.nkm03@gmail.com>
2024-06-21 18:31:35 +0100

    add props to starRating

--- src/components/StarRatings.js
+++ src/components/StarRatings.js
@@ -7,14 +7,31 @@ const starContainer = {
   display: "flex",
 }
 
-export default function StarRatings({ maxRating = 10 }) {
-  const [rating, setRating] = useState(1)
+export default function StarRatings({
+  maxRating = 10,
+  color = "#FFFF8F",
+  size = 48,
+  className = "",
+  defaultRating = 0,
+  setMovieRating,
+}) {
+  const [rating, setRating] = useState(defaultRating)
 
+  function handleSetRating(num) {
+    setRating(num)
+    setMovieRating && setMovieRating(num)
+  }
   return (
-    <div style={ratingContainer}>
+    <div style={ratingContainer} className={className}>
       <div style={starContainer}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} onClick={() => setRating(i + 1)} full={rating >= i + 1} />
+          <Star
+            key={i}
+            onClick={() => handleSetRating(i + 1)}
+            full={rating >= i + 1}
+            color={color}
+            size={size}
+          />
         ))}
       </div>
       <p>{rating || ""}</p>
@@ -22,22 +39,22 @@ export default function StarRatings({ maxRating = 10 }) {
   )
 }
 
-const starStyle = {
-  display: "block",
-  width: "48px",
-  height: "48px",
-  cursor: "pointer",
-}
+function Star({ onClick, full, size, color }) {
+  const starStyle = {
+    display: "block",
+    width: `${size}px`,
+    height: `${size}px`,
+    cursor: "pointer",
+  }
 
-function Star({ onClick, full }) {
   return (
     <span style={starStyle} role="button" onClick={onClick}>
       {full ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#FFFF8F" stroke="#000">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={color} stroke={color}>
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#000">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke={color}>
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
@@ -8,6 +8,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRatings maxRating={10} />
+    <StarRatings maxRating={10} color="#bed558" />
+    <Test />
   </React.StrictMode>
 )
+function Test() {
+  const [rating, setMovieRating] = useState(0)
+  return (
+    <>
+      <StarRatings setMovieRating={setMovieRating} maxRating={5} />
+      <p>It was rated {rating} stars</p>
+    </>
+  )
+}
